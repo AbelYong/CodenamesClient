db39a35387556df9ef0ef520c66e4419055abbea
AbelYong <zs23014089@estudiantes.uv.mx>
2025-09-21 21:11:17 -0600

    Language switching from start window implemented

--- StartWindow.xaml.cs
+++ StartWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,33 @@ namespace CodenamesClient
         public StartWindow()
         {
             InitializeComponent();
+        }
+
+        private void Click_btnEnglish(object sender, RoutedEventArgs e)
+        {
+            Properties.Settings.Default.languageCode = "en-US";
+            Properties.Settings.Default.Save();
+            Restart();
+        }
+
+        private void Click_btnSpanish(object sender, RoutedEventArgs e)
+        {
+            Properties.Settings.Default.languageCode = "es-MX";
+            Properties.Settings.Default.Save();
+            Restart();
+        }
+
+        private void Click_btnStart(object sender, RoutedEventArgs e)
+        {
+            //TODO
+            throw new NotImplementedException();
+        }
+
+        private static void Restart()
+        {
+            string executionPath = Process.GetCurrentProcess().MainModule.FileName;
+            Process.Start(executionPath);
+            Application.Current.Shutdown();
         }
     }
 }
