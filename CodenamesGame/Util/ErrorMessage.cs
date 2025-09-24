b6d75d89b9b1b9129ae9610c9eb6ebad2013d49f
AbelYong <zs23014089@estudiantes.uv.mx>
2025-09-24 13:08:15 -0600

    Created project for game logic, ErrorMessage class added

---
+++ CodenamesGame/Util/ErrorMessage.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodenamesGame.Util
+{
+    public class ErrorMessage
+    {
+        private bool _isError { get; set; }
+        private string _message { get; set; }
+
+        public ErrorMessage(bool isError, string message)
+        {
+            _isError = isError;
+            _message = message;
+        }
+
+        /// <summary>
+        /// This class should only be used to return
+        /// true to a caller who expects an ErrorMessage,
+        /// use 2 arguments constructor otherwise
+        /// </summary>
+        /// <param name="isError"></param>
+        public ErrorMessage(bool isError)
+        {
+            _isError = isError;
+        }
+    }
+}
