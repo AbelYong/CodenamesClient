c6a80ce35323e0a1855cd8fc32587de099ed24df
RazielFa <razi.kaiser11@gmail.com>
2025-11-07 10:45:56 -0600

    Added animation for word cards + slight modification to the board style

--- GameUI/BoardUI/BoardPage.xaml.cs
+++ GameUI/BoardUI/BoardPage.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -44,27 +45,47 @@ namespace CodenamesClient.GameUI.BoardUI
             NavigationService.GoBack();
         }
 
-        private void Click_Keyword(object sender, RoutedEventArgs e)
+        private async void Click_Keyword(object sender, RoutedEventArgs e)
         {
             if (sender is ToggleButton clickedButton)
             {
+                clickedButton.IsEnabled = false;
+
                 int row = Grid.GetRow(clickedButton);
                 int column = Grid.GetColumn(clickedButton);
                 int code = _viewModel.AgentsMatrix[row, column];
+                ImageBrush newBackground;
                 switch (code)
                 {
                     case AGENT_CODE:
-                        clickedButton.Background = GetAgentCardImage();
-                        clickedButton.Content = string.Empty;
-                        clickedButton.IsEnabled = false;
+                        newBackground = GetAgentCardImage();
+                        break;
+                    case BYSTANDER_CODE:
+                        newBackground = GetBystanderCardImage();
+                        break;
+                    case ASSASSIN_CODE:
+                        newBackground = GetAssassinCardImage();
+                        break;
+                    default:
+                        newBackground = new ImageBrush();
+                        break;
+                }
+
+                await AnimateCardFlip(clickedButton, isFlippingAway: true, durationMs: 150);
+
+                clickedButton.Background = newBackground;
+                clickedButton.Content = string.Empty;
+
+                await AnimateCardFlip(clickedButton, isFlippingAway: false, durationMs: 200);
+
+                switch (code)
+                {
+                    case AGENT_CODE:
                         _viewModel.StopTimer();
                         _viewModel.AddTime(_viewModel.TurnLength);
                         _viewModel.StartTimer();
                         break;
                     case BYSTANDER_CODE:
-                        clickedButton.Background = GetBystanderCardImage();
-                        clickedButton.Content = string.Empty;
-                        clickedButton.IsEnabled = false;
                         _viewModel.TurnTimer = 0;
                         _viewModel.StopTimer();
                         MessageBox.Show("Le diste a un civil, se ha terminado tu turno");
@@ -73,9 +94,6 @@ namespace CodenamesClient.GameUI.BoardUI
                         _viewModel.StartTimer();
                         break;
                     case ASSASSIN_CODE:
-                        clickedButton.Background = GetAssassinCardImage();
-                        clickedButton.Content = string.Empty;
-                        clickedButton.IsEnabled = false;
                         _viewModel.TurnTimer = 0;
                         _viewModel.StopTimer();
                         MessageBox.Show("Te encontraste con un asesino, fin del juego");
@@ -149,6 +167,127 @@ namespace CodenamesClient.GameUI.BoardUI
                     }
                 }
             }
+        }
+
+        private void AnimateBoardCards()
+        {
+            int delay = 0;
+            int delayIncrement = 20;
+            int animationDuration = 600;
+
+            double startTranslateX = -500;
+            double startTranslateY = 300;
+            double startRotation = -45;
+
+            foreach (UIElement element in gridBoard.Children)
+            {
+                if (element is ToggleButton card)
+                {
+                    var translate = new TranslateTransform(startTranslateX, startTranslateY);
+                    var rotate = new RotateTransform(startRotation);
+                    var scale = new ScaleTransform(1.0, 1.0);
+
+                    var group = new TransformGroup();
+                    group.Children.Add(translate);
+                    group.Children.Add(rotate);
+                    group.Children.Add(scale);
+
+                    card.RenderTransform = group;
+
+                    card.RenderTransformOrigin = new Point(0.5, 0.5);
+
+                    card.Opacity = 0;
+
+                    var storyboard = new Storyboard();
+
+                    var ease = new CubicEase { EasingMode = EasingMode.EaseOut };
+
+                    var opacityAnim = new DoubleAnimation(0, 1, TimeSpan.FromMilliseconds(animationDuration))
+                    {
+                        EasingFunction = ease,
+                        BeginTime = TimeSpan.FromMilliseconds(delay)
+                    };
+                    Storyboard.SetTarget(opacityAnim, card);
+                    Storyboard.SetTargetProperty(opacityAnim, new PropertyPath("Opacity"));
+                    storyboard.Children.Add(opacityAnim);
+
+                    var slideXAnim = new DoubleAnimation(startTranslateX, 0, TimeSpan.FromMilliseconds(animationDuration))
+                    {
+                        EasingFunction = ease,
+                        BeginTime = TimeSpan.FromMilliseconds(delay)
+                    };
+                    Storyboard.SetTarget(slideXAnim, card);
+                    Storyboard.SetTargetProperty(slideXAnim, new PropertyPath("(UIElement.RenderTransform).(TransformGroup.Children)[0].(TranslateTransform.X)"));
+                    storyboard.Children.Add(slideXAnim);
+
+                    var slideYAnim = new DoubleAnimation(startTranslateY, 0, TimeSpan.FromMilliseconds(animationDuration))
+                    {
+                        EasingFunction = ease,
+                        BeginTime = TimeSpan.FromMilliseconds(delay)
+                    };
+                    Storyboard.SetTarget(slideYAnim, card);
+                    Storyboard.SetTargetProperty(slideYAnim, new PropertyPath("(UIElement.RenderTransform).(TransformGroup.Children)[0].(TranslateTransform.Y)"));
+                    storyboard.Children.Add(slideYAnim);
+
+                    var rotateAnim = new DoubleAnimation(startRotation, 0, TimeSpan.FromMilliseconds(animationDuration))
+                    {
+                        EasingFunction = ease,
+                        BeginTime = TimeSpan.FromMilliseconds(delay)
+                    };
+                    Storyboard.SetTarget(rotateAnim, card);
+                    Storyboard.SetTargetProperty(rotateAnim, new PropertyPath("(UIElement.RenderTransform).(TransformGroup.Children)[1].(RotateTransform.Angle)"));
+                    storyboard.Children.Add(rotateAnim);
+
+                    storyboard.Begin();
+
+                    delay += delayIncrement;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Animates the card to “flip” using its ScaleTransform (at index 2 of the TransformGroup).
+        /// </summary>
+        /// <param name="card">The ToggleButton to be animated. </param>
+        /// <param name="isFlippingAway">True if it will shrink (1 to 0), False if it will expand (0 to 1). </param>
+        /// <param name="durationMs">Duration of the animation in milliseconds.</param>
+        /// <returns>A Task that completes when the animation ends.</returns>
+        private Task AnimateCardFlip(ToggleButton card, bool isFlippingAway, double durationMs = 150)
+        {
+            var transformGroup = card.RenderTransform as TransformGroup;
+            if (transformGroup == null || transformGroup.Children.Count <= 2 || !(transformGroup.Children[2] is ScaleTransform scaleTransform))
+            {
+                return Task.CompletedTask;
+            }
+
+            double fromValue = isFlippingAway ? 1.0 : 0.0;
+            double toValue = isFlippingAway ? 0.0 : 1.0;
+
+            var anim = new DoubleAnimation
+            {
+                From = fromValue,
+                To = toValue,
+                Duration = TimeSpan.FromMilliseconds(durationMs),
+                EasingFunction = isFlippingAway ? (IEasingFunction)new BackEase { EasingMode = EasingMode.EaseIn, Amplitude = 0.3 }
+                                                : new BackEase { EasingMode = EasingMode.EaseOut, Amplitude = 0.3 }
+            };
+
+            var tcs = new TaskCompletionSource<bool>();
+            anim.Completed += (s, e) =>
+            {
+                scaleTransform.BeginAnimation(ScaleTransform.ScaleXProperty, null);
+                scaleTransform.ScaleX = toValue;
+                tcs.SetResult(true);
+            };
+
+            scaleTransform.BeginAnimation(ScaleTransform.ScaleXProperty, anim);
+
+            return tcs.Task;
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            AnimateBoardCards();
         }
     }
 }
